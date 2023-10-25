dbb34eccf3fc9223f3ea85120cb8f9b81aada088
umair <uamjad508@gmail.com>
2023-10-25 18:24:40 +0500

    new key

--- controler/Events/Events.js
+++ controler/Events/Events.js
@@ -21,6 +21,26 @@ router.post('/events', AsyncError(async (req, res, next) => {
     const events = req.body.data
     const condition = events.allDay === true ? 1 : 0
     console.log(events)
+
+    // const timestampstart = events.start;
+    // const timestampend = events.end;
+
+// Create a new Date object and pass the timestamp in milliseconds
+// const date = new Date(timestampstart);
+// const date2 = new Date(timestampend);
+
+// Extract the components of the date and time
+// function formatDateTime(date) {
+//     const year = date.getFullYear();
+//     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const hours = String(date.getHours()).padStart(2, '0');
+//     const minutes = String(date.getMinutes()).padStart(2, '0');
+//     const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+//     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+//   }
+
     // return
     try {
         const sqlinsert = `INSERT INTO events (id, title, description, allDay, start, end, CollegeID, status) VALUES (NULL,'${events.title}','${events.description}','${condition}','${events.start}','${events.end}','${'1'}','${'1'}')`;
